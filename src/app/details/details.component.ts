d719d6796092db4dcee73c35617b809872b90d73
AkeemKhan <akeemkhan@outlook.com>
2018-12-01 14:07:35 +0000

    Styling + Mode drop down

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -14,11 +14,13 @@ export class DetailsComponent implements OnInit {
   PlayerRequest: PlayerDetails;
 
   Commands: Command[] = [
-    new Command('Black Ops 4', 1),
-    new Command('Rainbow Six Siege', 2)
+    new Command('Black Ops 4', 'bo4', ['Blackout', 'Multiplayer']),
+    new Command('Rainbow Six Siege', 'r6s', ['Ranked'])
   ];
 
   SelectedCommand: Command;
+  SelectedMode: string;
+  PossibleModes: string[];
 
   constructor() {
     this.PlayerRequest = new PlayerDetails();
@@ -27,14 +29,25 @@ export class DetailsComponent implements OnInit {
   ngOnInit() {
   }
 
+  onGameChange(command: Command) {
+    this.PossibleModes = command.Modes;
+    this.SelectedMode = this.PossibleModes[0];
+    console.log('Selected Game - ' + this.SelectedCommand.Display);
+  }
+
+  onModeChange(mode: string){
+    console.log('Selected Game - ' + this.SelectedCommand.Display + ' selected mode: ' + this.SelectedMode);
+  }
+
 }
 
 export class Command {
   Display: string;
-  Id: number;
-
-  constructor(disp: string, id: number) {
+  CommandId: string;
+  Modes: string[]
+  constructor(disp: string, id: string, modes: string[]) {
     this.Display = disp;
-    this.Id = id;
+    this.CommandId = id;
+    this.Modes = modes;
   }
 }
